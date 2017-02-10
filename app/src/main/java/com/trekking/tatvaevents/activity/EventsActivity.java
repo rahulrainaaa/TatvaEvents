bd471b45b5d5b825f52a50213e20aec0db783619
rahulrainahomeee <rahulrainahome@gmail.com>
2017-02-10 16:55:00 +0530

    Initial Commit

--- app/src/main/java/com/trekking/tatvaevents/activity/EventsActivity.java
+++ app/src/main/java/com/trekking/tatvaevents/activity/EventsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 
 import com.trekking.tatvaevents.R;
 import com.trekking.tatvaevents.adapter.EventsListAdapter;
+import com.trekking.tatvaevents.adapter.EventsPageAdapter;
 import com.trekking.tatvaevents.eventhandler.callbackhandler.EventItemCallback;
 import com.trekking.tatvaevents.eventhandler.listener.EventListViewItemListener;
 import com.trekking.tatvaevents.model.Event;
@@ -31,11 +33,12 @@ public class EventsActivity extends AppCompatActivity implements View.OnClickLis
     /**
      * Global data members.
      */
+    private DrawerLayout drawer = null;
     private ListView m_eventListView = null;
+    private ViewPager mEventViewPager = null;
     private FloatingActionButton m_callFab = null;
     private ArrayList<Event> m_eventsArrayList = null;
     private EventsListAdapter m_eveEventsListAdapter = null;
-    private DrawerLayout drawer = null;
     private EventListViewItemListener m_ItemEventListener = null;
 
     /**
@@ -55,12 +58,21 @@ public class EventsActivity extends AppCompatActivity implements View.OnClickLis
 
         m_eventListView = (ListView) findViewById(R.id.events_list_view);
         m_callFab = (FloatingActionButton) findViewById(R.id.calling_fab);
+        mEventViewPager = (ViewPager) findViewById(R.id.viewpager);
 
         m_eventsArrayList = new ArrayList<>();
         for (int nEventNo = 0; nEventNo < 30; nEventNo++) {
             Event event = new Event();
+            event.setSno(nEventNo);
             event.setTitle("Event Title " + nEventNo);
             event.setDateTime("2017-03-25 08:00 PM");
+            event.setDescription("Description:: " + nEventNo);
+            event.setPlace("Place:: " + nEventNo);
+            event.setPhone("Phone:: " + nEventNo);
+            event.setPrice("Price:: " + nEventNo);
+            event.setMapPlot("MapPlot:: " + nEventNo);
+            event.setBooking("Booking:: " + nEventNo);
+            event.setImage("image:: " + nEventNo);
             m_eventsArrayList.add(event);
         }
 
@@ -74,6 +86,9 @@ public class EventsActivity extends AppCompatActivity implements View.OnClickLis
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        mEventViewPager.setAdapter(new EventsPageAdapter(this, m_eventsArrayList));
+        mEventViewPager.setCurrentItem(0);
     }
 
     @Override
@@ -129,6 +144,7 @@ public class EventsActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onEventItemClicked(View view, int tag) {
 
+        mEventViewPager.setCurrentItem(tag);
         drawer.closeDrawers();
         Toast.makeText(this, "event item: " + tag, Toast.LENGTH_SHORT).show();
     }
---
+++ app/src/main/java/com/trekking/tatvaevents/adapter/EventsPageAdapter.java
@@ -0,0 +1,82 @@
+package com.trekking.tatvaevents.adapter;
+
+import android.app.Activity;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.trekking.tatvaevents.R;
+import com.trekking.tatvaevents.model.Event;
+
+import java.util.ArrayList;
+
+/**
+ * @class EventsPageAdapter
+ * @desc Adapter class for Event page View.
+ */
+public class EventsPageAdapter extends PagerAdapter {
+
+    /**
+     * Global data members.
+     */
+    private ArrayList<Event> mEventArrayList = null;
+    private LayoutInflater mInflater = null;
+    private Activity mActivity = null;
+
+    /**
+     * Constructor
+     */
+    public EventsPageAdapter(Activity activity, ArrayList<Event> list) {
+
+        this.mEventArrayList = list;
+        this.mActivity = activity;
+        this.mInflater = LayoutInflater.from(activity);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+
+        ViewGroup layout = (ViewGroup) mInflater.inflate(R.layout.event_page_view, collection, false);
+
+
+        collection.addView(layout);
+        return layout;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object view) {
+        container.removeView((View) view);
+        Toast.makeText(mActivity, "destroy:" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int getCount() {
+
+        if (mEventArrayList != null) {
+            return mEventArrayList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+
+    /**
+     * @method destroy
+     * @desc Method to destroy data and clear memory.
+     */
+    public void destroy() {
+
+    }
+}
